618b13df1563026387990155cab13579ec3f0eb0
root <root@localhost.localdomain>
2024-11-24 10:20:09 +0000

    finish basic submission

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './styles/reset.css';
+import './styles/style.css';
+import App from './pages/App';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
